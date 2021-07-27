5e40fcda021f0fe2985dc3874e823636e0a23293
Shents124 <72143563+Shents124@users.noreply.github.com>
2021-07-27 14:56:57 +0700

    add UI

---
+++ Assets/Dark UI/Scripts/QualityManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Michsky.UI.Dark
+{
+    public class QualityManager : MonoBehaviour
+    {
+        [Header("AUDIO")]
+        public AudioMixer mixer;
+        public SliderManager masterSlider;
+        public SliderManager musicSlider;
+        public SliderManager sfxSlider;
+
+        void Start()
+        {
+            mixer.SetFloat("Master", Mathf.Log10(PlayerPrefs.GetFloat(masterSlider.sliderTag + "DarkSliderValue")) * 20);
+            mixer.SetFloat("Music", Mathf.Log10(PlayerPrefs.GetFloat(musicSlider.sliderTag + "DarkSliderValue")) * 20);
+            mixer.SetFloat("SFX", Mathf.Log10(PlayerPrefs.GetFloat(sfxSlider.sliderTag + "DarkSliderValue")) * 20);
+        }
+        
+        public void AnisotropicFilteringEnable()
+        {
+            QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
+        }
+
+        public void AnisotropicFilteringDisable()
+        {
+            QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+        }
+
+        public void AntiAliasingSet(int index)
+        {
+            // 0, 2, 4, 8 - Zero means off
+            QualitySettings.antiAliasing = index;
+        }
+
+        public void VsyncSet(int index)
+        {
+            // 0, 1 - Zero means off
+            QualitySettings.vSyncCount = index;
+        }
+
+        public void ShadowResolutionSet(int index)
+        {
+            if (index == 3)
+                QualitySettings.shadowResolution = ShadowResolution.VeryHigh;
+            else if (index == 2)
+                QualitySettings.shadowResolution = ShadowResolution.High;
+            else if (index == 1)
+                QualitySettings.shadowResolution = ShadowResolution.Medium;
+            else if (index == 0)
+                QualitySettings.shadowResolution = ShadowResolution.Low;
+        }
+
+        public void TextureSet(int index)
+        {
+            // 0 = Full, 4 = Eight Resolution
+            QualitySettings.masterTextureLimit = index;
+        }
+        
+        public void ReflectionSet(int index)
+        {
+            if (index == 0)
+                QualitySettings.realtimeReflectionProbes = false;
+            else if (index == 1)
+                QualitySettings.realtimeReflectionProbes = true;
+        }
+
+        public void VolumeSetMaster(float volume)
+        {
+            mixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+        }
+
+        public void VolumeSetMusic(float volume)
+        {
+            mixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        }
+
+        public void VolumeSetSFX(float volume)
+        {
+            mixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        }
+
+       
+    }
+}
